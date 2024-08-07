be2da1ca12839104f00f7487d8508244b9ee7fa3
Shafia Ansar <shafiaansar142@gmail.com>
2024-08-07 07:37:09 +0500

    grafbase integration

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <section className="flex-start flex-col paddings mb-16">
       <h1>Categories</h1>
       <h1>Posts</h1>
-      <h1>Load more</h1>
+      <h1>Load more
+        
+      </h1>
     </section>
   );
 }
